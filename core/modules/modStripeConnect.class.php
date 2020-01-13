87358362410748da1f47f09b032f4cb3bb48caa2
ptibogxiv <support@ptibogxiv.net>
2020-01-13 17:34:14 +0100

    Update modStripeConnect.class.php

--- core/modules/modStripeConnect.class.php
+++ core/modules/modStripeConnect.class.php
@@ -163,7 +163,7 @@ class modStripeConnect extends DolibarrModules {
 			"DELETE FROM ".MAIN_DB_PREFIX."const WHERE name = ".$this->db->encrypt('STRIPE_TEST_SECRET_KEY', 1)." AND entity != '0' ",
       "DELETE FROM ".MAIN_DB_PREFIX."const WHERE name = ".$this->db->encrypt('STRIPE_LIVE_PUBLISHABLE_KEY', 1)." AND entity != '0' ",
 			"DELETE FROM ".MAIN_DB_PREFIX."const WHERE name = ".$this->db->encrypt('STRIPE_LIVE_SECRET_KEY', 1)." AND entity != '0' ",
-      "DELETE FROM ".MAIN_DB_PREFIX." WHERE entity = '0' AND url = '/stripeconnect/account.php' and position = '104'"
+      "DELETE FROM ".MAIN_DB_PREFIX."menu WHERE entity = '0' AND url = '/stripeconnect/account.php' and position = '104'"
 		);
 
 		return $this->_remove($sql, $options);

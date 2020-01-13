b2831622a1e8b44cfd563774ec9f08a105dd51fb
ptibogxiv <support@ptibogxiv.net>
2020-01-13 22:14:01 +0100

    Update account.php

--- account.php
+++ account.php
@@ -324,7 +324,8 @@ print '</td></tr>'."\n";
 
 print '<tr class="oddeven"><td><label for="name">'.$langs->trans("MerchantCategoryCode").'</label></td><td>';
 print $langs->trans($account->business_type);
-print ' - '.$account->business_profile->mcc;
+print ' - '.$langs->getLabelFromKey($db, $account->business_profile->mcc, 'c_merchantcategorycodes', 'code', 'label');
+print " (".$account->business_profile->mcc.")";
 //<input name="nom" id="name" class="minwidth200" value="'. dol_escape_htmltag($conf->global->MAIN_INFO_SOCIETE_NOM?$conf->global->MAIN_INFO_SOCIETE_NOM: GETPOST("nom", 'nohtml')) . '"'.(empty($conf->global->MAIN_OPTIMIZEFORTEXTBROWSER) ? '' : ' autofocus="autofocus"').'>
 print '</td></tr>'."\n";
 
